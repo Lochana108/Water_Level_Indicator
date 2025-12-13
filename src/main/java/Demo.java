1f9f2ec537b0b1ca10c44f1162a7bab3e49745b5
Lochana108 <deshanlochana2@gmail.com>
2025-12-13 13:23:41 +0530

    Updated project using interface

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -5,72 +5,87 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.Random;
 
-class WaterLevelObserver extends JFrame {
-    WaterLevelObserver(String title){
-        super(title);
-        setSize(300,300);
+interface WaterLevelObserver {
+    public void update(int waterLevel);
+}
+
+class Alarm extends JFrame implements WaterLevelObserver {
+    private JLabel lblAlarm;
+
+    Alarm() {
+        super("Alarm Window");
+        setSize(300, 300);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new FlowLayout());
-    }
-    public void update(int waterLevel) {
-
-    }
-}
-
-class Alarm extends WaterLevelObserver {
-    private JLabel lblAlarm;
-    Alarm(){
-        super("Alarm Window");
         lblAlarm = new JLabel("");
-        lblAlarm.setFont(new Font("",1,25));
+        lblAlarm.setFont(new Font("", 1, 25));
         add(lblAlarm);
         setVisible(true);
     }
+
     public void update(int waterLevel) {
-        lblAlarm.setText(waterLevel>=50 ? "ON":"OFF");
+        lblAlarm.setText(waterLevel >= 50 ? "ON" : "OFF");
     }
 }
 
-class Splitter extends WaterLevelObserver {
+class Splitter extends JFrame implements WaterLevelObserver {
     private JLabel lblSplitter;
-    Splitter(){
+
+    Splitter() {
         super("Splitter Window");
+        setSize(300, 300);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new FlowLayout());
         lblSplitter = new JLabel("");
-        lblSplitter.setFont(new Font("",1,25));
+        lblSplitter.setFont(new Font("", 1, 25));
         add(lblSplitter);
         setVisible(true);
     }
+
     public void update(int waterLevel) {
-        lblSplitter.setText(waterLevel>=50?"Splitter ON":"Splitter OFF");
+        lblSplitter.setText(waterLevel >= 50 ? "Splitter ON" : "Splitter OFF");
     }
 }
 
-class Display extends WaterLevelObserver {
+class Display extends JFrame implements WaterLevelObserver {
     private JLabel lblDisplay;
-    Display(){
+
+    Display() {
         super("Display Window");
+        setSize(300, 300);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new FlowLayout());
         lblDisplay = new JLabel("");
-        lblDisplay.setFont(new Font("",1,25));
+        lblDisplay.setFont(new Font("", 1, 25));
         add(lblDisplay);
         setVisible(true);
     }
+
     public void update(int waterLevel) {
-        lblDisplay.setText("Water Level is : "+waterLevel);
+        lblDisplay.setText("Water Level is : " + waterLevel);
     }
 }
 
-class SMSSender extends WaterLevelObserver {
+class SMSSender extends JFrame implements WaterLevelObserver {
     private JLabel SMSLable;
-    SMSSender(){
+
+    SMSSender() {
         super("SMS Window");
+        setSize(300, 300);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new FlowLayout());
         SMSLable = new JLabel("");
-        SMSLable.setFont(new Font("",1,25));
+        SMSLable.setFont(new Font("", 1, 25));
         add(SMSLable);
         setVisible(true);
     }
+
     public void update(int waterLevel) {
-        SMSLable.setText("Sending : "+waterLevel);
+        SMSLable.setText("Sending : " + waterLevel);
     }
 }
 
@@ -79,7 +94,7 @@ class WaterLevelObserverble {
 
     private ArrayList<WaterLevelObserver> observerList = new ArrayList<>();
 
-    public void addWaterLevelObserver(WaterLevelObserver ob) {
+    public void addWaterLevelObserver(WaterLevelObserver ob){
         observerList.add(ob);
     }
 
@@ -96,23 +111,25 @@ class WaterLevelObserverble {
         }
     }
 }
+
 class WaterTank extends JFrame {
     private JSlider waterLevelSlider;
     private WaterLevelObserverble waterLevelObserverble;
 
-    WaterTank(){
-        setSize(300,300);
+    WaterTank() {
+        setTitle("Water Tank");
+        setSize(300, 300);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new FlowLayout());
 
-        waterLevelObserverble =new WaterLevelObserverble();
+        waterLevelObserverble = new WaterLevelObserverble();
         waterLevelObserverble.addWaterLevelObserver(new Alarm());
         waterLevelObserverble.addWaterLevelObserver(new Splitter());
         waterLevelObserverble.addWaterLevelObserver(new Display());
         waterLevelObserverble.addWaterLevelObserver(new SMSSender());
 
-        waterLevelSlider = new JSlider(JSlider.VERTICAL,0,100,50);
+        waterLevelSlider = new JSlider(JSlider.VERTICAL, 0, 100, 50);
         waterLevelSlider.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e) {
                 int waterLevel = waterLevelSlider.getValue();
@@ -121,12 +138,11 @@ class WaterTank extends JFrame {
         });
         add(waterLevelSlider);
     }
-
 }
+
 public class Demo {
     public static void main(String[] args) {
         new WaterTank().setVisible(true);
-
 
     }
 }
